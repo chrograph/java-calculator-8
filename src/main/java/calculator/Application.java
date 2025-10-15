eb7031e9bd4d08aad260f5767b1f82d28820258d
unknown <chrograph@naver.com>
2025-10-15 18:34:14 +0900

    양수값을 저장하는 메서드 제작

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,9 +5,13 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        String delimiter = classifyDelimiter(input);
-        System.out.println(delimiter);
 
+        String delimiter = classifyDelimiter(input);
+
+        int[] delimiterIndexes = getDelimiterIndexes(delimiter, input);
+
+        String IntegerArray = getIntegerArray(input, delimiterIndexes);
+        System.out.println(IntegerArray);
     }
 
     private static String classifyDelimiter(String input) {
@@ -32,6 +36,40 @@ public class Application {
             throw new IllegalArgumentException();
         }
     }
+
+    private static int[] getDelimiterIndexes(String delimeter, String input) {
+        int[] indexes = new int[input.length()];
+
+        int count = 0;
+
+        int pos = input.indexOf(delimeter);
+        while (pos != -1) {
+            indexes[count++] = pos;
+            pos = input.indexOf(delimeter, pos + delimeter.length());
+        }
+        return indexes;
+    }
+
+    private static String getIntegerArray(String input, int[] delimiterIndexes) {
+        String integerArray;
+        int count = 0;
+
+        int startIndex = 0;
+        if(input.contains("\\n")) {
+            startIndex = input.indexOf("\\n") + 2;
+            count++;
+        }
+
+        integerArray = input.substring(startIndex, delimiterIndexes[count]);
+        while (delimiterIndexes[count+1] != 0) {
+            integerArray = integerArray.concat(input.substring(delimiterIndexes[count] + 1, delimiterIndexes[count + 1]));
+            count++;
+        }
+        integerArray = integerArray.concat(input.substring(delimiterIndexes[count] + 1));
+
+        return integerArray;
+    }
+
 }
 
 //   //;\n1;2;3
