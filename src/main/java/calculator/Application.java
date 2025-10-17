f1cf84be2b89bdfd73771399e6b6aeb69413f38f
unknown <chrograph@naver.com>
2025-10-18 01:11:51 +0900

    1,2:3 케이스 고려 및 리펙토링

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,83 +2,50 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Arrays;
-
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
-        String delimiter = classifyDelimiter(input);
+        String changeInput = changeDelimiter(input);
 
         if(input.contains("\\n")) {
-            input = deleteCustomDelimiter(input);
+            changeInput = deleteCustomDelimiter(changeInput);
         }
 
-        int[] delimiterIndexes = getDelimiterIndexes(delimiter, input);
-
-        String integerArray = getIntegerArray(input, delimiterIndexes);
+        String[] splitInput =  changeInput.split(",");
 
         int sum = 0;
-        for (int i = 0; i < integerArray.length(); ++i) {
-            sum += Integer.parseInt(integerArray.substring(i, i + 1));
+        for (int i = 0; i < splitInput.length; ++i) {
+            sum += Integer.parseInt(splitInput[i]);
         }
         System.out.println("결과 : " + sum);
     }
 
-    private static String classifyDelimiter(String input) {
+
+    //수정
+    private static String changeDelimiter(String input) {
         if (input.contains("//") && input.contains("\\n")) {
-            return getCustomDelimiter(input);
-        } else if (input.contains(",")) {
-            return ",";
+            return changeCustomDelimiter(input);
         } else if (input.contains(":")) {
-            return ":";
+            return input.replaceAll(":", ",");
+        } else if (input.contains(",")) {
+            return input;
         } else {
             throw new IllegalArgumentException();
         }
     }
 
-    private static String getCustomDelimiter(String input) {
+    private static String changeCustomDelimiter(String input) {
         int start = input.indexOf("//") + 2;
         int end = input.indexOf("\\n");
 
         if (start == end - 1) {
-            return input.substring(start, end);
+            String delimiter = input.substring(start, end);
+            return input.replaceAll(delimiter, ",");
         } else {
             throw new IllegalArgumentException();
         }
-    }
-
-    private static int[] getDelimiterIndexes(String delimeter, String input) {
-        int[] indexes = new int[input.length()];
-        Arrays.fill(indexes, -1);
-
-        int count = 0;
-
-        int pos = input.indexOf(delimeter);
-        while (pos != -1) {
-            indexes[count++] = pos;
-            pos = input.indexOf(delimeter, pos + delimeter.length());
-        }
-        return indexes;
-    }
-
-    private static String getIntegerArray(String input, int[] delimiterIndexes) {
-        if (delimiterIndexes[0] == -1) {
-            return input;
-        }
-
-        String integerArray;
-
-        int count = 0;
-        integerArray = input.substring(0, delimiterIndexes[count]);
-        while ((count + 1) < delimiterIndexes.length && delimiterIndexes[count + 1] != -1) {
-            integerArray = integerArray.concat(input.substring(delimiterIndexes[count] + 1, delimiterIndexes[count + 1]));
-            count++;
-        }
-        integerArray = integerArray.concat(input.substring(delimiterIndexes[count] + 1));
-
-        return integerArray;
     }
 
     private static String deleteCustomDelimiter(String input) {
