fc48d154bd1b71360e5b228ed7306b64db351586
unknown <chrograph@naver.com>
2025-10-20 00:14:20 +0900

    예외 처리 리펙토링

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,17 +9,26 @@ public class Application {
 
         String changeInput = changeDelimiter(input);
 
-        if(input.contains("\\n")) {
-            changeInput = deleteCustomDelimiter(changeInput);
-        }
-
         String[] splitInput =  changeInput.split(",");
+
+        for (String token : splitInput) {
+            checkException(token);
+        }
 
         int sum = 0;
         for (int i = 0; i < splitInput.length; ++i) {
             sum += Integer.parseInt(splitInput[i]);
         }
         System.out.println("결과 : " + sum);
+    }
+
+    private static void checkException(String token) {
+        if (token.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        if (token.startsWith("-")) {
+            throw new IllegalArgumentException();
+        }
     }
 
 
@@ -36,12 +45,14 @@ public class Application {
         }
     }
 
+    //수정
     private static String changeCustomDelimiter(String input) {
         int start = input.indexOf("//") + 2;
         int end = input.indexOf("\\n");
 
         if (start == end - 1) {
             String delimiter = input.substring(start, end);
+            input = deleteCustomDelimiter(input);
             return input.replaceAll(delimiter, ",");
         } else {
             throw new IllegalArgumentException();
